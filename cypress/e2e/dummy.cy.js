57ec9c6479f6be80a7e4ed9ad37ea4d713370108
sukhadshrestha <25838061+SukhadShrestha@users.noreply.github.com>
2025-06-18 16:40:47 +0545

    dummy

--- cypress/e2e/dummy.cy.js
+++ cypress/e2e/dummy.cy.js
@@ -3,7 +3,7 @@ describe('Add task', () => {
       cy.visit('/')
       cy.wait(3000)
       cy.getCyAttr('add-task-name').type('Random Task')
-      cy.getCyAttr('add-no-of-period').type('2')
+      cy.getCyAttr('add-no-of-perio').type('2')
       cy.getCyAttr('add-task-submit').click()
       cy.getCyAttr('task-table').should('contain', 'Random Task')
     })
